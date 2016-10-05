1e825a7195895f6fec8509e49302da98494cc525
ivangerard <ivan.gerard@hotmail.com>
2016-10-05 18:03:21 +0700

    bootstrap

--- client/routes/index.js
+++ client/routes/index.js
@@ -5,7 +5,11 @@ var router = express.Router()
 //export router
 
 router.get('/', function(req, res, next) {
-    res.render('master')
+    res.render('pages/home')
+})
+
+router.get('/items', function(req, res, next) {
+    res.render('pages/items')
 })
 
 module.exports = router
