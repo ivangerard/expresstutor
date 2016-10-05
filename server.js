22c442a96a39d6f0ac23ef4543d63a4c1619edb4
ivangerard <ivan.gerard@hotmail.com>
2016-10-05 15:27:10 +0700

    Fix conflict

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
- var express = require('express')
- var mongoose = require('mongoose');
- var bodyParser = require('body-parser');
- var app = express()
- var port = process.env.PORT || 8080;
+var express = require('express')
+var cors = require('cors')
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var app = express()
+var port = process.env.PORT || 8080;
 
- mongoose.connect('mongodb://localhost/expresstutor')
+mongoose.connect('mongodb://localhost/expresstutor')
 
- app.use(bodyParser.json())
- app.use(bodyParser.urlencoded({
-     extended: true
- }))
- app.use('/api', require('./routes/api'))
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
+app.use('/api', require('./routes/api'))
 
- app.listen(port);
- console.log('The magic happens on port ' + port);
+app.listen(port);
+console.log('The magic happens on port ' + port);
